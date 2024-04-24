f68e2a7675f4b8d755358a6d1df49988acab2d39
matheusfy <matheusy@alunos.utfpr.edu.br>
2024-04-24 20:14:39 -0300

    add connection with postgres [ALURA]

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.postgresql:postgresql:42.1.4")
     implementation( "com.google.code.gson:gson:2.10.1")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
